ca6d8560a8554225dc7d406185ba68f52e868232
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-04-12 20:59:01 +0200

    new link

--- src/components/ReturnBtn.jsx
+++ src/components/ReturnBtn.jsx
@@ -3,7 +3,7 @@ import { Button } from '@mui/material'
 
 const ReturnBtn = () => {
     return (
-        <a href="https://63d14f15b8305d4d556107bf--startling-bienenstitch-220eca.netlify.app/?fbclid=IwAR15QoCcO4CvjPsP6s9XFXjzsA-U0MZzl_LF9BaMIUGoS9jBG0GNkjygFkY" sx={{zIndex:'2'}}>
+        <a href="https://main--startling-bienenstitch-220eca.netlify.app" sx={{zIndex:'2'}}>
             <Button variant='outlined' size='medium' sx={{position: 'sticky', zIndex: '2', left: '100px', top: '50px'}}>
                 Wróć do portfolio
             </Button>
